d1b7f22bb75ba15238777faab29661f6fd9ba3e2
chips-gguma <ljygguma@gmail.com>
2022-01-02 19:59:30 +0900

    POST 방식에서의 인증 성공 csrf() disable (SPA 페이지에서 동작하기 위함)

--- server/basic-authentication-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ server/basic-authentication-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -24,6 +24,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                // POST 방식은 csrf filter 작동하므로 disable 시켜주어야 POST 인증이 가능하다.
+                // SPA 페이지에서 RestController의 서비스를 하려고 할 때 위와 같이 해주어야 하는데
+                // 서버가 동시에 웹페이지도 서비스를 해서 csrf를 enable() 시켜야 한다면 어떻게 해야 할까?
+                .csrf().disable()
                 .authorizeRequests().anyRequest().authenticated() // 모든 request에 대해 authenticated()로 막음
                 .and()
                 .httpBasic()    // 그리고 httpBasic()으로 들어와
--- server/basic-authentication-test/src/test/java/com/sp/fc/web/BasicAuthenticationTest.java
+++ server/basic-authentication-test/src/test/java/com/sp/fc/web/BasicAuthenticationTest.java
@@ -3,6 +3,7 @@ package com.sp.fc.web;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -54,6 +55,23 @@ public class BasicAuthenticationTest {
         ResponseEntity<String> res = client.exchange(greetingUrl(), HttpMethod.GET, entity, String.class);// get 방식으로 entity를 가지고 와서 String으로 data 가져 옴
 
         assertEquals("hello", res.getBody());
+    }
+
+    @DisplayName("3. 인증 성공 2")
+    @Test
+    void test_3(){
+        // TestRestTemplate은 바로 basicHeaderToken을 넣어서 request를 날려 줌
+        TestRestTemplate testClient = new TestRestTemplate("user1", "1111");
+        String res = testClient.getForObject(greetingUrl(), String.class);
+        assertEquals("hello", res);
+    }
+
+    @DisplayName("4. POST 인증")
+    @Test
+    void test_4(){
+        TestRestTemplate testClient = new TestRestTemplate("user1", "1111");
+        ResponseEntity<String> res  = testClient.postForEntity(greetingUrl(), "jeongyeon", String.class);
+        assertEquals("hello jeongyeon", res.getBody());
     }
 
 }
